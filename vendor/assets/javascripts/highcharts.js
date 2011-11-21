7d6883b1a9f656a2f16fc8c8a9875c9ec4f0c2cb
Per Christian B. Viken <perchr@northblue.org>
2011-11-21 19:05:58 +0100

    Automatic whitespace fix

--- vendor/assets/javascripts/highcharts.js
+++ vendor/assets/javascripts/highcharts.js
@@ -792,7 +792,7 @@ if (!globalAdapter && win.jQuery) {
 
 
 	//=== Extend jQuery on init
-	
+
 	/*jslint unparam: true*//* allow unused param x in this function */
 	jQ.extend(jQ.easing, {
 		easeOutQuad: function (x, t, b, c, d) {
@@ -804,25 +804,25 @@ if (!globalAdapter && win.jQuery) {
 	// extend the animate function to allow SVG animations
 	var jFx = jQuery.fx,
 		jStep = jFx.step;
-		
+
 	// extend some methods to check for elem.attr, which means it is a Highcharts SVG object
 	each(['cur', '_default', 'width', 'height'], function (fn, i) {
 		var obj = i ? jStep : jFx.prototype, // 'cur', the getter' relates to jFx.prototype
 			base = obj[fn],
 			elem;
-		
+
 		if (base) { // step.width and step.height don't exist in jQuery < 1.7
-		
+
 			// create the extended function replacement
 			obj[fn] = function (fx) {
-				
+
 				// jFx.prototype.cur does not use fx argument
 				fx = i ? fx : this;
-				
+
 				// shortcut
 				elem = fx.elem;
-				
-				// jFX.prototype.cur returns the current value. The other ones are setters 
+
+				// jFX.prototype.cur returns the current value. The other ones are setters
 				// and returning a value has no effect.
 				return elem.attr ? // is SVG element wrapper
 					elem.attr(fx.prop, fx.now) : // apply the SVG wrapper's method
@@ -830,7 +830,7 @@ if (!globalAdapter && win.jQuery) {
 			};
 		}
 	});
-	
+
 	// animate paths
 	jStep.d = function (fx) {
 		var elem = fx.elem;
